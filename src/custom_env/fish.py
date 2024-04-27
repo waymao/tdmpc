253b170c0b5a7d3be0851fa1f493e95f00b4d3e8
yixiangsun <yixiang_sun@brown.edu>
2024-04-27 00:38:58 -0400

    new fish swim

--- src/custom_env/fish.py
+++ src/custom_env/fish.py
@@ -36,6 +36,39 @@ def swim_dir(time_limit=_DEFAULT_TIME_LIMIT, random=None, environment_kwargs=Non
       physics, task, control_timestep=_CONTROL_TIMESTEP, time_limit=time_limit,
       **environment_kwargs)
 
+@fish.SUITE.add('custom')
+def swim_new(time_limit=_DEFAULT_TIME_LIMIT, random=None, environment_kwargs=None):
+  """Returns the Fish Swim task."""
+  physics = fish.Physics.from_xml_string(*get_model_and_assets())
+  task = Swim_new(random=random)
+  environment_kwargs = environment_kwargs or {}
+  return control.Environment(
+      physics, task, control_timestep=_CONTROL_TIMESTEP, time_limit=time_limit,
+      **environment_kwargs)
+
+class Swim_new(fish.Swim):
+
+  def __init__(self, random=None):
+
+    super().__init__(random=random)
+    target_angle = np.random.uniform(-np.pi, np.pi)
+    self.swim_dir_x = np.cos(target_angle)
+    self.swim_dir_y = np.sin(target_angle)
+
+  def get_observation(self, physics):
+    """Returns an observation of joints, target direction and velocities."""
+    obs = collections.OrderedDict()
+    obs['target'] = physics.mouth_to_target()
+    obs['joint_angles'] = physics.joint_angles()
+    obs['upright'] = physics.upright()
+    obs['velocity'] = physics.velocity()
+    obs['swim_dir', 'x'] = self.swim_dir_x
+    obs['swim_dir', 'y'] = self.swim_dir_y
+    obs['mouth_pos'] = physics.named.data.geom_xpos['mouth']
+    obs['mouth_mat'] = physics.named.data.geom_xmat['mouth']
+    return obs
+
+
 class Swim_dir(fish.Swim):
   """A Fish `Task` for swimming with smooth reward."""
 
@@ -81,10 +114,11 @@ class Swim_dir(fish.Swim):
                     bounds=(self._desired_speed, self._desired_speed*2),
                     margin=self._desired_speed)
     dir_ = np.array([self.swim_dir_x, self.swim_dir_y, 0])
+    cos_dir = np.dot(physics.velocity()[:3]/speed, dir_)
     dir_reward = rewards.tolerance(
-                    dir_,
+                    cos_dir,
                     bounds=(0.9, 1.0),
-                    margin=2)
+                    margin=1)
     is_upright = 0.5 * (physics.upright() + 1)
       
     return (dir_reward * move_reward * 7 + is_upright) / 8.0
